400ff89c288d6eabee7728d9dd7979f87a18120b
Jaden Ali San Diego <jaden.sandiego@gmail.com>
2022-08-23 13:03:12 +0800

    I finally made the button owrk. I only need to fix a minor bug about it decreasing to negative integer. I will add in future versions the give up functionality, the ability to customize the timer using user input, and minor appearance improvements.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const startingMinutes = 10;
+const startingMinutes = 0.1;
 let time = startingMinutes * 60;
 const countdownEl = document.getElementById('countdown');
+isStopped = false;
 function updateTimer() {
     const minutes = Math.floor(time/60);
     let seconds = time % 60;
@@ -13,5 +14,7 @@ function updateTimer() {
     countdownEl.innerHTML = `${minutes}: ${seconds}`;
     time--;
 }
-setInterval(updateTimer, 1000);
-
+function startTimer()
+{
+    setInterval(updateTimer,1000);
+}
